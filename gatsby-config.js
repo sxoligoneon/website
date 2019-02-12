7bbd5d4758c073360d27be533b05459909bb2460
TheodorosPloumis <me@theodorosploumis.com>
2019-02-12 22:15:36 +0200

    Build with pathPrefix

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ module.exports = {
     description: 'Ο επίσημος ιστότοπος της Σχολής Γονέων ΙΜ Σερρών & Νιγρίτης με εισηγήτρια της κ. Χάρη Δημητρακούδη.',
     siteUrl: `https://www.sxoligoneon.gr`
   },
-  pathPrefix: '/sxoligoneon',
+  pathPrefix: '/website',
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass',
